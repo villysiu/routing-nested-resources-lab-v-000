332b1d90e726252ca00425ebba905e6e149df36a
Villy Siu <villysiu@gmail.com>
2017-11-07 21:26:57 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       @song = Song.find_by(id: params[:id])
-  
+
       if @artist.songs.include?(@song)
         @song
       else
