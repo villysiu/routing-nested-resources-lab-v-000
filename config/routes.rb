b52ff5343801fba2b9a90ba51868a16995f241bc
Villy Siu <villysiu@gmail.com>
2017-11-07 19:21:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:show] do
+  resources :artists, only: [:index, :show] do
     # nested resource for posts
     resources :songs, only: [:show, :index]
   end
